1c08c7d0b1b4283128016da6e0d21bd89a649053
Joshua Wu <joshua.wu.0910@gmail.com>
2017-07-22 18:59:27 -0700

    updated test to remove files from save folder and original folder

--- tests/create_file.py
+++ tests/create_file.py
@@ -3,5 +3,7 @@ import os, time
 
 # test files
 os.system('echo > ../watch-folder/test.txt')
-time.sleep(5)
+time.sleep(3)
 os.system('rm ../watch-folder/test.txt')
+os.system('rm ../original-folder/test.txt')
+os.system('rm ../save-folder/test.txt')
