45a362caf1369812702ee4196dc25c73ad2813c9
Joshua Wu <joshua.wu.0910@gmail.com>
2017-07-27 15:46:58 -0700

    updated test file to copy test image for testing

--- tests/create_file.py
+++ tests/create_file.py
@@ -2,17 +2,16 @@ import os, time
 
 
 # test files
-print 'Creating test file'
-os.system('echo > ../watch-folder/test.txt')
+print 'Copying test image to watch folder...'
+os.system('cp testimagejpg.jpg ../watch-folder/testimagejpg.jpg')
+
+# allow time for watch.py to process
 time.sleep(3)
 
-print 'Cleaning up test.txt and test images...'
-os.system('rm ../watch-folder/test.txt')
-os.system('rm ../original-folder/test.txt')
-os.system('rm ../save-folder/test.txt')
-os.system('rm ../print-folder/test.txt')
-
+print 'Cleaning up test images...'
 os.system('rm ../watch-folder/testimagejpg.jpg')
 os.system('rm ../original-folder/testimagejpg.jpg')
-os.system('rm ../save-folder/testimagejpg.jpg')
-os.system('rm ../print-folder/testimagejpg.jpg')
+os.system('rm ../save-folder/Maywood2017testimagejpg.jpg')
+os.system('rm ../print-folder/Maywood2017testimagejpg.jpg')
+
+print 'Tests complete...'
