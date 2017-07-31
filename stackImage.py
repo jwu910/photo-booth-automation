a4e57cae95cb21e5894622df6c3a11a28bf2251f
Joshua Wu <joshua.wu.0910@gmail.com>
2017-07-30 22:27:25 -0700

    Updated doc string and some comments

--- stackImage.py
+++ stackImage.py
@@ -11,24 +11,24 @@ saveFolder = 'save-folder'
 
 def stackImage(currentImage, overlayImage, processAllPictures):
 	"""
-	stackImage takes two arguments, args[0] is the base image and args[1] is the image to superimpose over the base.
+	stackImage takes three arguments, args[0] is the base image, args[1] is the image to superimpose over the base, and args[2] will define whether printing is added in to the default work flow or not.
 	This function will take two images, and return one image.
 	"""
 	fileInfo = currentImage.split('/')
 
 	print 'stackImage started...'
 	# Base image
-	print 'DEBUG backround image = ' + currentImage
 	background = Image.open(currentImage)
-	print currentImage + ' is your base image'
+	print currentImage + ' is set as base image'
 
 	# Overlay image
 	overlay = Image.open(overlayImage)
 
+	# Stack two images
 	background.paste(overlay, (0, 0), overlay)
 	background.save(saveFolder + '/' + prefixText + fileInfo[2])
 
-	# If processAllPictures all pictures set to True
+	# If processAllPictures all pictures set to True, save a copy to the print-folder.
 	if processAllPictures:
 		background.save(printFolder + '/' + prefixText + fileInfo[2])
 
